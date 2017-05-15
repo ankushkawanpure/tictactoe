f9afc8c637d9467467ac8061551ced5083bffd47
ankushkawanpure <ankushkawanpure@gmail.com>
2017-05-14 18:04:01 -0700

    material ui added with tap plugin

--- src/containers/Template.js
+++ src/containers/Template.js
@@ -2,22 +2,31 @@
  * Created by Ankush on 5/13/17.
  */
 import React, {Component} from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import RaisedButton from 'material-ui/RaisedButton';
+
+injectTapEventPlugin();
 
 class Template extends Component {
 
 
+
     render() {
         return (
-            <div>
-                <header>
-                    <h1>
-                    TIc Tac Toe
-                    </h1>
-                </header>
-                <main>
-                    {this.props.children}
-                </main>
-            </div>
+            <MuiThemeProvider>
+                <div>
+                    <RaisedButton onTouchTap={() => {alert("YOu clicked")}} label="Primary" primary={true}  />
+                    <header>
+                        <h1>
+                            TIc Tac Toe
+                        </h1>
+                    </header>
+                    <main>
+                        {this.props.children}
+                    </main>
+                </div>
+            </MuiThemeProvider>
         )
     }
 
