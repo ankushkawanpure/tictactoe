6afabd35ff964cfeab1d09a033a0b16aad110520
ankushkawanpure <ankushkawanpure@gmail.com>
2017-05-14 17:34:58 -0700

    React router version fix

--- src/config/endpoint.js
+++ src/config/endpoint.js
@@ -1,4 +1,4 @@
 /**
  * Created by Ankush on 5/14/17.
  */
-export const relayApi = 'https://api.graph.cool/relay/v1/ciyn8rxsl3bs201324sbzf0hx';
+export const relayApi = 'https://api.graph.cool/relay/v1/cj2pdk3sxp7480175qw76eo6h';
