0090c5a5162a3be26b1fbc196862786e16269db1
ankushkawanpure <ankushkawanpure@gmail.com>
2017-05-14 17:47:53 -0700

    home and profile route added

---
+++ src/containers/Profile.js
@@ -0,0 +1,21 @@
+/**
+ * Created by Ankush on 5/14/17.
+ */
+import React, {Component} from 'react';
+
+class Profile extends Component {
+
+
+    render() {
+        return (
+            <div>
+                <h2> Profile </h2>
+            </div>
+        )
+    }
+
+
+}
+
+
+export default Profile;
